c7fa5471f99c67ffc07154a75751e12b7202387b
Greg Roberts <gregvroberts@outlook.com>
2021-06-03 10:02:14 -0500

    Created article struct

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+/**
+ * @Description: Declare global articles array that we can populate in
+ * our main function to simulate a database
+ */
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
 
 func main() {
-	fmt.Print("Hello, World!")
+
+	//populate Articles with dummy data
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description 2", Content: "Article Content 2"},
+	}
+
+	handleRequests()
 }
