289bc6c023f6e90171ae8919ba3ea9b85c053051
Greg Roberts <gregvroberts@outlook.com>
2021-06-03 11:46:10 -0500

    Added CRUD methods to main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -37,6 +38,54 @@ func readAllArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Articles)
 }
 
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	//get body of post request
+	//return the string response containing the request body
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(requestBody, &article)
+
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+	//fmt.Fprintf(w, "%+v", string(requestBody))
+
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	id := vars["id"]
+
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our
+		// articles
+		if article.Id == id {
+			// updates our Articles array to remove the
+			// article
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+
+}
+
+//todo
+//func updateArticle(w http.ResponseWriter, r *http.Request) {
+//	requestBody, _ := ioutil.ReadAll(r.Body)
+//	vars := mux.Vars(r)
+//	id := vars["id"]
+//	var article Article
+//	json.Unmarshal(requestBody, &article)
+//	for index, article := range Articles {
+//		if article.Id == id {
+//
+//		}
+//	}
+//}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
@@ -44,10 +93,18 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	gorillaRouter := mux.NewRouter().StrictSlash(true)
-
+	//HomePage
 	gorillaRouter.HandleFunc("/", homePage)
+	// return all articles
+	gorillaRouter.HandleFunc("/articles", readAllArticles)
+	//create a single article
+	//MUST BE BEFORE THE OTHER /article ENDPOINT
+	gorillaRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	// delete article
+	gorillaRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	//return a single article
 	gorillaRouter.HandleFunc("/article/{id}", readSingleArticle)
-	gorillaRouter.HandleFunc("/all", readAllArticles)
+	// Start up server
 	log.Fatal(http.ListenAndServe(":3000", gorillaRouter))
 }
 
