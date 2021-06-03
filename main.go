d393d00b4793726d50310cbdced2dae06e9abca3
Greg Roberts <gregvroberts@outlook.com>
2021-06-03 10:18:34 -0500

    Created returnSingleArticle

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 type Article struct {
+	Id      string `json:"Id"`
 	Title   string `json:"title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
@@ -18,22 +21,41 @@ type Article struct {
  */
 var Articles []Article
 
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint his: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	gorillaRouter := mux.NewRouter().StrictSlash(true)
+
+	gorillaRouter.HandleFunc("/", homePage)
+	gorillaRouter.HandleFunc("/all", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":3000", gorillaRouter))
 }
 
 func main() {
-
+	fmt.Println("Rest API v2.0 - Mux Routers")
 	//populate Articles with dummy data
 	Articles = []Article{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description 2", Content: "Article Content 2"},
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description 2", Content: "Article Content 2"},
 	}
 
 	handleRequests()
